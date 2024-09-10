25f1041eb60944257867f54c5656e55487660a3b
amal.ballouchi@gmail.com <amal.ballouchi@gmail.com>
2024-09-10 21:24:20 +0100

    Add validation messages to the methods Add Sale and Update Quantity of the stock

--- Application/UseCases/UpdateQuantityBySaler.cs
+++ Application/UseCases/UpdateQuantityBySaler.cs
@@ -10,10 +10,12 @@ namespace Application.UseCases
     public class UpdateQuantityBySaler
     {
         private readonly ISalerRepository _salerRepository;
+        private readonly IBeerRepository _beerRepository;
 
-        public UpdateQuantityBySaler(ISalerRepository salerRepository)
+        public UpdateQuantityBySaler(ISalerRepository salerRepository, IBeerRepository beerRepository)
         {
             _salerRepository = salerRepository;
+            _beerRepository = beerRepository;
         }
 
         public async Task Execute(int salerId, int beerId, int newQuantity)
@@ -21,8 +23,11 @@ namespace Application.UseCases
             var saler = await _salerRepository.GetSalerByIdAsync(salerId);
             if (saler == null) throw new Exception("Saler does not exist");
 
-            var salerStock = saler.salerStocks.FirstOrDefault(ss => ss.BeerId == beerId);
-            if (salerStock == null) throw new Exception("Beer does not exist");
+            var beer = await _beerRepository.GetBeerByIdAsync(beerId);
+            if (beer == null) throw new Exception("Beer does not exist");
+
+            var salerStock = saler.salerStocks.FirstOrDefault(ss => ss.BeerId == beerId && ss.SalerId == salerId);
+            if (salerStock == null) throw new Exception("Beer does not bellong to this saler");
 
             salerStock.Quantity = newQuantity;
 
