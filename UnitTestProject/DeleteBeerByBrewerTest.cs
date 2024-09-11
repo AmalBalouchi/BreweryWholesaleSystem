931f507f5a3fb831a6dc84d665b48b5e03ce7c44
amal.ballouchi@gmail.com <amal.ballouchi@gmail.com>
2024-09-11 17:37:48 +0100

    Implement Unit tests

---
+++ UnitTestProject/DeleteBeerByBrewerTest.cs
@@ -0,0 +1,56 @@
+﻿using Application.Services;
+using Application.UseCases;
+using Domain.Entities;
+using Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    public class DeleteBeerByBrewerTest
+    {
+        [Fact]
+        public async Task DeleteBeerByBrewer_Valid()
+        {
+            // Arrange
+            var mockBeerRepository = new Mock<IBeerRepository>();
+            var useCase = new DeleteBeerByBrewer(mockBeerRepository.Object);
+            var beerId = 1;
+            var brewerId = 2;
+
+            // Act
+            await useCase.ExecuteAsync(beerId, brewerId);
+
+            // Assert
+            mockBeerRepository.Verify(s => s.DeleteBeerByBrewer(beerId, brewerId), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteBeerByBrewer_InvalidBeerException()
+        {
+            // Arrange
+            var mockBeerRepository = new Mock<IBeerRepository>();
+            var beerId = 1;
+            var brewerId = 2;
+
+            // Setup the repository to throw an exception when trying to delete a non-existent beer
+            mockBeerRepository.Setup(r => r.DeleteBeerByBrewer(beerId, brewerId))
+                              .ThrowsAsync(new Exception("Beer does not exist"));
+
+            var useCase = new DeleteBeerByBrewer(mockBeerRepository.Object);
+
+            // Act
+            var exception = await Assert.ThrowsAsync<Exception>(() => useCase.ExecuteAsync(beerId, brewerId));
+
+            // Assert
+            Assert.Equal("Beer does not exist", exception.Message);
+
+            // Verify that DeleteBeerByBrewer was called once
+            mockBeerRepository.Verify(r => r.DeleteBeerByBrewer(beerId, brewerId), Times.Once);
+        }
+    }
+}
---
+++ UnitTestProject/GetBeersByBrewerTest.cs
@@ -0,0 +1,39 @@
+﻿using Application.UseCases;
+using Domain.Entities;
+using Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    public class GetBeersByBrewerTest
+    {
+        [Fact]
+        public async Task AddBeerByBrewer_Valid()
+        {
+            // Arrange
+            var mockBeerRepository = new Mock<IBeerRepository>();
+            var useCase = new GetBeersByBrewer(mockBeerRepository.Object);
+            var brewerId = 1;
+
+            var expectedBeers = new List<Beer>
+            {
+                new Beer { Id = 1, Name = "Beer1", BrewerId = brewerId },
+                new Beer { Id = 2, Name = "Beer2", BrewerId = brewerId }
+            };
+
+            // Setup the repository to return the expected beers
+            mockBeerRepository.Setup(r => r.GetBeersByBrewer(brewerId)).ReturnsAsync(expectedBeers);
+
+            // Act
+            var result = await useCase.ExecuteAsync(brewerId);
+
+            // Assert
+            Assert.Equal(expectedBeers, result);
+        }
+    }
+}
